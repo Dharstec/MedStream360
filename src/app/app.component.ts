cd16996c749f68cec075825b40f442e33354788e
Mugesh1 <mugeshmuggi933@gmail.com>
2023-10-17 20:52:19 -0700

    Medstream

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+// app.component.ts
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'newWebDes';
+export class AppComponent implements OnInit {
+  contentLoaded = false;
+  hoverButton = false;
+  buttonMove = 0;
+
+  ngOnInit() {
+    // Simulate content loading after a delay
+    setTimeout(() => {
+      this.contentLoaded = true;
+    }, 3000); // Adjust the delay as needed
+  }
+
+  onMouseOverButton() {
+    if (this.hoverButton) {
+      this.buttonMove = 10; // Adjust the distance to move
+    }
+  }
+
+  onMouseOutButton() {
+    if (this.hoverButton) {
+      this.buttonMove = 0;
+    }
+  }
 }
